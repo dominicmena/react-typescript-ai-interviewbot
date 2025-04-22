10472b3e0c75602ec39ea8ca056ad70208c48cd6
dominicmena <mena.dominic@gmail.com>
2025-04-21 22:42:54 -0500

    added card to authForm and titles

--- app/components/AuthForm.tsx
+++ app/components/AuthForm.tsx
@@ -15,6 +15,7 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import Image from "next/image"
 
 const formSchema = z.object({
   username: z.string().min(2).max(50),
@@ -39,7 +40,13 @@ const AuthForm = () => {
 
   return (
     <div className="card-border lg:min-w-[566px]">  
-    
+    <div className="flex flex-col gap-6 card py-14 px-10">
+      <div className="flex flex-row gap-2 justify-center">
+        <Image src="/logo.svg" alt="logo" height = {32} width={38}/>
+        <h2 className="text-primary-100">PrepWise</h2>
+      </div>
+      <h3>Practice Interviews with AI</h3>
+    </div>
     
     <Form {...form}>
     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
